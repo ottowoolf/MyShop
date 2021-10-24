7d61e8a44e57a885c438f3572b8c961812a85719
OtmanO <otto.woolf@gmail.com>
2021-10-24 22:53:54 +1100

    Initial Github commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
-function App() {
-  return <></>
+import React, { Fragment } from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+
+const App = () => {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Container>
+          <h1>Welcome to My E-Commerce</h1>
+        </Container>
+      </main>
+      <Footer />
+    </Fragment>
+  )
 }
 
 export default App
