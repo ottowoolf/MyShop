a30d64ea5327a7fabfbda859866f8421c3e5d9c7
otto <otto.woolf@gmail.com>
2021-11-27 22:48:22 +1100

    Added product create/update functionality

--- frontend/src/screens/ProductEditScreen.js
+++ frontend/src/screens/ProductEditScreen.js
@@ -6,7 +6,8 @@ import Message from "../components/Message"
 import Loader from "../components/Loader"
 import FormContainer from "../components/FormContainer"
 
-import { listProductDetails } from "../actions/productActions"
+import { listProductDetails, updateProduct } from "../actions/productActions"
+import { PRODUCT_UPDATE_RESET } from "../constants/productConstants"
 
 const ProductEditScreen = ({ match, history }) => {
   const productId = match.params.id
@@ -24,24 +25,47 @@ const ProductEditScreen = ({ match, history }) => {
   const productDetails = useSelector((state) => state.productDetails)
   const { loading, error, product } = productDetails
 
+  const productUpdate = useSelector((state) => state.productUpdate)
+  const {
+    loading: loadingUpdate,
+    error: errorUpdate,
+    success: successUpdate,
+  } = productUpdate
+
   useEffect(() => {
-    if (!product.name || product._id !== productId) {
-      dispatch(listProductDetails(productId))
+    if (successUpdate) {
+      dispatch({ type: PRODUCT_UPDATE_RESET })
+      history.push("/admin/productlist")
     } else {
-      setName(product.name)
-      setPrice(product.price)
-      setImage(product.image)
-      setBrand(product.brand)
-      setCategory(product.category)
-      setCountInStock(product.countInStock)
-      setDescription(product.description)
+      if (!product.name || product._id !== productId) {
+        dispatch(listProductDetails(productId))
+      } else {
+        setName(product.name)
+        setPrice(product.price)
+        setImage(product.image)
+        setBrand(product.brand)
+        setCategory(product.category)
+        setCountInStock(product.countInStock)
+        setDescription(product.description)
+      }
     }
-  }, [product, productId, dispatch, history])
+  }, [product, productId, dispatch, history, successUpdate])
 
   const submitHandler = (e) => {
     e.preventDefault()
 
-    //TODO update product
+    dispatch(
+      updateProduct({
+        _id: productId,
+        name,
+        price,
+        image,
+        brand,
+        category,
+        countInStock,
+        description,
+      })
+    )
   }
 
   return (
@@ -51,6 +75,8 @@ const ProductEditScreen = ({ match, history }) => {
       </Link>
       <FormContainer center>
         <h1>Edit Product</h1>
+        {loadingUpdate && <Loader />}
+        {errorUpdate && <Message>{errorUpdate}</Message>}
         {loading ? (
           <Loader />
         ) : error ? (
