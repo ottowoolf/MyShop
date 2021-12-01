bb2b812ef498683900fad06ed2a8dafce87e3cba
otto <otto.woolf@gmail.com>
2021-12-01 22:53:20 +1100

    small styling changes

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -7,7 +7,7 @@ const Product = ({ product }) => {
   return (
     <Card className="p-2 rounded myCard h-100">
       <Link to={`/product/${product._id}`}>
-        <Card.Img src={product.image} variant="top" />
+        <Card.Img src={product.image} variant="top" height="200px" />
       </Link>
       <Card.Body>
         <Link to={`/product/${product._id}`}>
